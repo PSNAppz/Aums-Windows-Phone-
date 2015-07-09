22b2a20481ef0ee37d2ee51881058a9ba21b263b
PSNAppZ <psnappz@gmail.com>
2015-07-09 21:29:17 +0530

    AUMS Beta 1.0

---
+++ AUMS/MainPage.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports System
+Imports System.Threading
+Imports System.Windows.Controls
+Imports Microsoft.Phone.Controls
+Imports Microsoft.Phone.Shell
+
+
+
+
+Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+
+
+
+    ' Constructor
+    Public Sub New()
+        InitializeComponent()
+
+        SupportedOrientations = SupportedPageOrientation.Portrait Or SupportedPageOrientation.Landscape
+
+        Web.Navigate(New Uri("https://aums-students-am.amrita.edu:8443/cas/login?service=https%3A%2F%2Faums-students-am.amrita.edu%3A8443%2Faums%2FJsp%2FCore_Common%2Findex.jsp", UriKind.RelativeOrAbsolute)
+
+    End Sub
+
+    ' Sample code for building a localized ApplicationBar
+    'Private Sub BuildLocalizedApplicationBar()
+    '    ' Set the page's ApplicationBar to a new instance of ApplicationBar.
+    '    ApplicationBar = New ApplicationBar()
+
+    '    ' Create a new button and set the text value to the localized string from AppResources.
+    '    Dim appBarButton As New ApplicationBarIconButton(New Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative))
+    '    appBarButton.Text = AppResources.AppBarButtonText
+    '    ApplicationBar.Buttons.Add(appBarButton)
+
+    '    ' Create a new menu item with the localized string from AppResources.
+    '    Dim appBarMenuItem As New ApplicationBarMenuItem(AppResources.AppBarMenuItemText)
+    '    ApplicationBar.MenuItems.Add(appBarMenuItem)
+    'End Sub
+
+
+   
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+End Class
